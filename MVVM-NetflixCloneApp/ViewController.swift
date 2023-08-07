0b62805e3ca83f52fc3d5f648e1125f87d22bfed
HALUK BAYRAKCI <halukbayrakci32@gmail.com>
2023-08-07 14:34:09 +0300

    Initial Commit

---
+++ MVVM-NetflixCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-NetflixCloneApp
+//
+//  Created by HALUK BAYRAKCI on 7.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
